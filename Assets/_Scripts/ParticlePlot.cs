3df3d4db51ebffb332a43d07617349cf3015b09a
Galen Ballew <pballe2@uic.edu>
2016-08-01 21:57:17 -0500

    disabled combinemeshinstance. disabled particle alpha blend shader - applied GPU instancing unlit shader. FPS as low as 20 when looking at entire field of vision (all the points). need to add materialblockproperties to add coloring.

--- Assets/_Scripts/ParticlePlot.cs
+++ Assets/_Scripts/ParticlePlot.cs
@@ -69,47 +69,23 @@ public class ParticlePlot : MonoBehaviour
             var normal = new Vector4(p.v[0],p.v[1],p.v[2],p.v[3]);
             normal.Normalize();
 
-            var meshParticle = new GameObject();
+              var meshParticle = Instantiate(Resources.Load("Point") as GameObject);
+            //var meshParticle = new GameObject();
             meshParticle.name = String.Format("Sphere {0}",i);
 
             meshParticle.transform.SetParent(meshParent.transform);
             meshParticle.transform.position = normal.StereographicProjection();
-            meshParticle.AddComponent<MeshFilter>().mesh = sphereMesh;
-            meshParticle.AddComponent<MeshRenderer>().material = sphereMaterial;
             meshParticle.transform.localScale = new Vector3(R,R,R);
-            meshParticle.GetComponent<Renderer>().material.SetColor("_TintColor", Color.white);
+            // meshParticle.AddComponent<MeshFilter>().mesh = sphereMesh;
+            // meshParticle.AddComponent<MeshRenderer>().material = sphereMaterial;
+            // meshParticle.GetComponent<Renderer>().material.SetColor("_TintColor", Color.white);
             i++;
         }
         _infoText.text = "";
-        combineMeshes(meshParent);
+
     }
 
-    public void combineMeshes(GameObject obj)
-  {
-    //Zero transformation is needed because of localToWorldMatrix transform
-    Vector3 position = obj.transform.position;
-    obj.transform.position = Vector3.zero;
 
-    //whatever man
-    MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
-    CombineInstance[] combine = new CombineInstance[meshFilters.Length];
-    int i = 0;
-    while (i < meshFilters.Length) {
-        combine[i].mesh = meshFilters[i].sharedMesh;
-        combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
-        meshFilters[i].gameObject.SetActive(false);
-        i++;
-    }
-    obj.transform.GetComponent<MeshFilter>().mesh = new Mesh();
-    obj.transform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine, true, true);
-    obj.transform.gameObject.SetActive(true);
-
-    //Reset position
-    obj.transform.position = position;
-
-    //Adds collider to mesh
-    obj.AddComponent<MeshCollider> ();
-  }
 
     public void Update()
     {
